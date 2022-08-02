9a9db512d66567e91435547a7709f1b829e304a7
anit135 <lazkris135@gmail.com>
2022-08-02 14:25:36 +0200

    fix active nav link

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,7 +29,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 py-lg-0">
                     <li class="nav-item"><a class="nav-link {{ request()->routeIs('index') ? 'active' : '' }}" href="{{ route('index') }}">Home</a></li>
-                    <li class="nav-item"><a class="nav-link {{ request()->routeIs('show_all_articles') ? 'active' : '' }}" href="{{ route('show_all_articles') }}">Catalog</a></li>
+                    <li class="nav-item"><a class="nav-link {{ request()->routeIs('show_all_articles') || request()->routeIs('show_article') ? 'active' : '' }}" href="{{ route('show_all_articles') }}">Catalog</a></li>
                 </ul>
             </div>
         </div>
